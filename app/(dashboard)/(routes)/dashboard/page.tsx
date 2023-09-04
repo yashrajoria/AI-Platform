369273a40bc45776e19227484a67545121dc4841
yashrajoria <yashrajoria@gmail.com>
2023-09-04 23:42:27 +0530

    added navbar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
     return (
         <div>
             <p>Dashboard Page</p>
-            <UserButton afterSignOutUrl="/" />
         </div>
     )
 }
