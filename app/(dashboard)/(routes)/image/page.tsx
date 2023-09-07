374f944c58a06ff96ba1bba01e245c22649aa47a
yashrajoria <yashrajoria@gmail.com>
2023-09-07 15:41:11 +0530

    Image generation image not loading error fix

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -53,14 +53,15 @@ const ImagePage = () => {
 
             //?TEST FROM HERE
 
-            console.log(typeof response);
-            console.log(typeof response.data);
-            const dataArray = [response.data]
+            // console.log(typeof response);
+            // console.log(typeof response.data);
+            // const dataArray = [response.data]
 
             // Assuming response.data is already an array of objects with a 'url' property
-            const urls = dataArray.map((image: { url: string }) => image.url);
+            const urls = response.data.map((image: { url: string }) => image.url);
             console.log("URLs setting in state", urls)
             setImages(urls);
+            form.reset()
         } catch (error) {
             console.error("[API Request Error]", error);
             // Handle errors here
@@ -179,7 +180,7 @@ const ImagePage = () => {
                                 <div className="relative aspect-square">
                                     <Image alt="Image"
                                         fill
-                                        src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-g9zMWVpfHyCvXae1Mhqd1LiJ/user-T4GzapuYMeZUPxvGJct4kPsa/img-VQv883WCSCPpq8IeoMlOZ8AM.png?st=2023-09-07T07%3A13%3A44Z&se=2023-09-07T09%3A13%3A44Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-09-06T23%3A49%3A55Z&ske=2023-09-07T23%3A49%3A55Z&sks=b&skv=2021-08-06&sig=o8rD5NSpaO%2BAfUuRj9wdy43QMcwELpqZyzTG3WJ2nmw%3D" />
+                                        src={src} />
                                 </div>
                                 <CardFooter className="p-2">
                                     <Button
--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -9,7 +9,6 @@ const openai = new OpenAI({
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
-    console.log({ userId });
     const body = await req.json();
     const { prompt, amount = 1, resolution = "512x512" } = body;
 
@@ -17,38 +16,34 @@ export async function POST(req: Request) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
+    if (!process.env.OPENAI_API_KEY) {
+      return new NextResponse("OpenAI API Key not configured.", {
+        status: 500,
+      });
+    }
+
     if (!prompt) {
       return new NextResponse("Prompt is required", { status: 400 });
     }
+
     if (!amount) {
       return new NextResponse("Amount is required", { status: 400 });
     }
+
     if (!resolution) {
       return new NextResponse("Resolution is required", { status: 400 });
     }
-    try {
-      const response = await openai.images.generate({
-        prompt,
-        n: parseInt(amount, 10),
-        size: resolution,
-      });
-      console.log({ response });
-      console.log(typeof response.data);
-      console.log(response.data);
 
-      // Access the message from the first choice
-      const dataArray = Object.values(response.data);
-      const message = dataArray[0];
-      return new NextResponse(message, { status: 200 });
-    } catch (error) {
-      console.error((error as Error).stack);
+    const response = await openai.images.generate({
+      prompt,
+      n: parseInt(amount, 10),
+      size: resolution,
+    });
 
-      // Log the error stack trace
-      console.error("[API Request Error]", error);
-      return new NextResponse("Error making API request", { status: 500 });
-    }
+    console.log(response);
+    return new NextResponse(JSON.stringify(response.data), { status: 200 });
   } catch (error) {
-    console.log("[Image_error]", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
+    console.log("[IMAGE_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }
