e41c97f34a5f09cae1f30c04269335948fb52940
yashrajoria <yashrajoria@gmail.com>
2023-09-07 23:00:11 +0530

    Added counter for API limit on sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,16 +1,18 @@
 import NavBar from '@/components/NavBar'
 import Sidebar from '@/components/Sidebar'
+import { getApiLimitCount } from '@/lib/api-limit'
 import React from 'react'
 
-const DashBoardLayout = ({
+const DashBoardLayout = async ({
     children
 }: {
     children: React.ReactNode
 }) => {
+    const apiLimitCount = await getApiLimitCount();
     return (
         <div className='h-full relative'>
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-                <div className='text-white'><Sidebar /></div>
+                <div className='text-white'><Sidebar apiLimitCount={apiLimitCount} /></div>
             </div>
             <main className='md:pl-72'>
                 <NavBar />
