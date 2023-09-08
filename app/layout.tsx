53ac8366b5bcc733819385dfa8026424b572031c
yashrajoria <yashrajoria@gmail.com>
2023-09-08 18:46:48 +0530

    Added CRISP - customer chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import ModalProvider from '@/components/ModalProvider'
 import { ToasterProvider } from '@/components/ToastProvider'
+import { CrispProvider } from '@/components/CrispProvider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <CrispProvider />
         <body className={inter.className}>
           <ToasterProvider />
           <ModalProvider />
